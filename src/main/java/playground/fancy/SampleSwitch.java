5ad671524721f30a7c633572ae5c5fb15813de8d
hjhessmann <hans.joerg.hessmann@anderscore.com>
2023-09-26 17:30:08 +0200

    Adds java21

--- src/main/java/playground/fancy/SampleSwitch.java
+++ src/main/java/playground/fancy/SampleSwitch.java
@@ -3,11 +3,14 @@ package playground.fancy;
 public class SampleSwitch {
 
     public String render(Object obj) {
-        return switch (obj) {
-            case String s -> s;
-            case Number n -> String.valueOf(n);
-            case null -> "nothing";
-            default -> throw new IllegalStateException("Unexpected value: " + obj);
-        };
+        if (obj == null) {
+            return "nothing";
+        } else if (obj instanceof Number) {
+            return String.valueOf(obj);
+        } else if (obj instanceof String) {
+            return (String) obj;
+        } else {
+            throw new IllegalStateException("Unexpected value: " + obj);
+        }
     }
 }
